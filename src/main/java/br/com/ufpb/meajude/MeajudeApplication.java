990557d8f1f7cd894055bc72a7b622f0239beb4b
Filipe de Lima Vaz Monteiro <42976390+filipelimavaz@users.noreply.github.com>
2023-09-20 17:01:51 -0300

    "spring initializr creation"

---
+++ src/main/java/br/com/ufpb/meajude/MeajudeApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufpb.meajude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeajudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeajudeApplication.class, args);
+	}
+
+}
