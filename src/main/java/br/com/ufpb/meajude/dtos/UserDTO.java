283736221dfcf82c39b0e770843ab9fde019ae87
Filipe de Lima Vaz Monteiro <42976390+filipelimavaz@users.noreply.github.com>
2023-09-26 20:59:03 -0300

    finishing user crud

--- src/main/java/br/com/ufpb/meajude/dtos/UserDTO.java
+++ src/main/java/br/com/ufpb/meajude/dtos/UserDTO.java
@@ -2,9 +2,11 @@ package br.com.ufpb.meajude.dtos;
 
 import br.com.ufpb.meajude.entities.Campaign;
 import br.com.ufpb.meajude.entities.User;
+import br.com.ufpb.meajude.entities.enums.Role;
 import br.com.ufpb.meajude.entities.enums.UserType;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 
 import java.util.List;
 
@@ -15,6 +17,7 @@ public class UserDTO {
     private String username;
     private UserType userType;
     private String phone;
+    private Role role;
     private List<Campaign> campaignList;
 
     public static UserDTO from(User user) {
@@ -23,6 +26,7 @@ public class UserDTO {
         userDTO.email = user.getEmail();
         userDTO.userType = user.getUserType();
         userDTO.phone = user.getPhone();
+        userDTO.role = user.getRole();
         userDTO.campaignList = user.getCampaignList();
         return userDTO;
     }
--- src/main/java/br/com/ufpb/meajude/services/UserService.java
+++ src/main/java/br/com/ufpb/meajude/services/UserService.java
@@ -2,10 +2,13 @@ package br.com.ufpb.meajude.services;
 
 import br.com.ufpb.meajude.dtos.UserDTO;
 import br.com.ufpb.meajude.dtos.UserRegistrationDTO;
+import br.com.ufpb.meajude.dtos.UserUpdateDTO;
 import br.com.ufpb.meajude.entities.User;
 import br.com.ufpb.meajude.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +39,7 @@ public class UserService {
             user.setUserType(userRegistrationDTO.getUserType());
             user.setIdentityDocument(userRegistrationDTO.getIdentityDocument());
             user.setPhone(userRegistrationDTO.getPhone());
+            user.setRole(userRegistrationDTO.getRole());
             userRepository.save(user);
 
             return UserDTO.from(user);
@@ -61,5 +65,32 @@ public class UserService {
             userDTOList.add(userDTO);
         }
         return userDTOList;
+    }
+
+    public UserDTO updateUser(String email, UserUpdateDTO userUpdateDTO) {
+        Optional<User> optionalUser = userRepository.findByEmail(email);
+
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            userUpdateDTO.update(user);
+            userRepository.save(user);
+            UserDTO updatedUserDTO = UserDTO.from(user);
+            userRepository.save(user);
+            return updatedUserDTO;
+        }
+        return null;
+
+    }
+
+    public UserDTO deleteUser(String email) {
+        Optional<User> optionalUser = userRepository.findByEmail(email);
+
+        if(optionalUser.isPresent()) {
+            User user = optionalUser.get();
+
+            userRepository.delete(user);
+            return UserDTO.from(user);
+        }
+        return UserDTO.from(optionalUser.get());
     }
 }
