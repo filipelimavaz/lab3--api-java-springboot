5ab473527c070f1ddd03a31c69c9459b59c2f66e
brunduar <bruno.duarte@dcx.ufpb.br>
2023-10-23 09:21:51 -0300

    fix: authentication controller

--- src/main/java/br/dcx/ufpb/meajude/controllers/AuthenticationController.java
+++ src/main/java/br/dcx/ufpb/meajude/controllers/AuthenticationController.java
@@ -5,6 +5,11 @@ import br.dcx.ufpb.meajude.dtos.user.LoginResponseDTO;
 import br.dcx.ufpb.meajude.dtos.user.UserRegistrationDTO;
 import br.dcx.ufpb.meajude.repositories.UserRepository;
 import br.dcx.ufpb.meajude.services.AuthorizationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jdk.jfr.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,11 +29,19 @@ public class AuthenticationController {
     private AuthorizationService authorizationService;
 
     @PostMapping("/login")
+    @Operation(summary = "Authenticates the properly user", description = "Email and password", tags = { "Authentication"}, responses = {
+            @ApiResponse(responseCode = "200", description = "User authenticated"),
+            @ApiResponse(responseCode = "401", description = "Invalid login")
+    })
     public ResponseEntity<LoginResponseDTO> loginUser(@RequestBody AuthenticationDTO authenticationDTO) {
         return new ResponseEntity<>(authorizationService.loginUser(authenticationDTO), HttpStatus.OK);
     }
 
     @PostMapping("/sign-up")
+    @Operation(summary = "User sign-up", description = "Sign up a new user", tags = { "Authentication" }, responses = {
+            @ApiResponse(responseCode = "201", description = "User created successfully"),
+            @ApiResponse(responseCode = "400", description = "You must provide a different email")
+    })
     public ResponseEntity<UserRegistrationDTO> registerUser(@RequestBody UserRegistrationDTO userRegistrationDTO) {
         return new ResponseEntity<>(authorizationService.registerUser(userRegistrationDTO), HttpStatus.CREATED);
     }
